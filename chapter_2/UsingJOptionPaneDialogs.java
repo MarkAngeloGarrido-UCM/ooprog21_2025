c3e1541666453f0b164dd3480c07518c982e0eea
MarkAngeloGarrido-UCM <markgelogarrido@gmail.com>
2025-09-08 10:36:25 +0800

    Update UsingJOptionPaneDialogs.java

--- chapter_2/UsingJOptionPaneDialogs.java
+++ chapter_2/UsingJOptionPaneDialogs.java
@@ -1,10 +1,13 @@
-package Chapter_2;
-
 import javax.swing.JOptionPane;
 
 public class UsingJOptionPaneDialogs {
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog(null, "What is your name?");
-        JOptionPane.showMessageDialog(null, "Hello, " + name);
+
+        int choice = JOptionPane.showConfirmDialog(null, "Do you want to display your name?");
+
+        if (choice == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "Hello, " + name);
+        }
     }
 }
