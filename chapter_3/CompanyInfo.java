f9439e86f4e1b12506d8c7c3bf80e049e3e8a500
MarkAngeloGarrido-UCM <markgelogarrido@gmail.com>
2025-09-19 09:01:58 +0800

    Create CompanyInfo.java

---
+++ chapter_3/CompanyInfo.java
@@ -0,0 +1,12 @@
+class CompanyInfo{
+   public static void displayHours(){
+      System.out.println("Monday - Friday 8 am to 6 pm\nSaturday        8 am to noon\nSunday          closed");
+   }
+   
+   public static void main(String[] args){
+      System.out.println("Smart Electronics");
+      displayHours();
+   }
+   
+} 
+
